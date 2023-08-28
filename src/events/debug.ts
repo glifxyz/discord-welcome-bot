54d2d684f0368ac01055623f302bb5535122fee0
Jamie Dubs <jamie@jamiedubs.com>
2023-08-28 18:05:18 +0200

    Silence debug events too

--- src/events/debug.ts
+++ src/events/debug.ts
@@ -2,6 +2,6 @@ import { event } from "jellycommands";
 
 export default event({
   name: "debug",
-  run: (_, info) => console.log("debug", info),
-  // run: (_, info) => {},
+  // run: (_, info) => console.log("debug", info),
+  run: (_, info) => {},
 });
