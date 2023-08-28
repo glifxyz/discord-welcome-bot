549782f4eac70d114ae364b929f543f9cf672f2f
Jamie Dubs <jamie@jamiedubs.com>
2023-08-28 10:18:24 +0200

    My vscode prettier did all this, plus update pkg lockfile

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-import 'dotenv/config';
-import { JellyCommands } from 'jellycommands';
-import { IntentsBitField } from 'discord.js';
+import "dotenv/config";
+import { JellyCommands } from "jellycommands";
+import { IntentsBitField } from "discord.js";
 
 const client = new JellyCommands({
-    // https://jellycommands.dev/guide/commands/loading
-    commands: 'src/commands',
+  // https://jellycommands.dev/guide/commands/loading
+  commands: "src/commands",
 
-    // https://jellycommands.dev/guide/events/loading
-    events: 'src/events',
+  // https://jellycommands.dev/guide/events/loading
+  events: "src/events",
 
-    // https://jellycommands.dev/guide/buttons/loading
-    buttons: 'src/buttons',
+  // https://jellycommands.dev/guide/buttons/loading
+  buttons: "src/buttons",
 
-    clientOptions: {
-        intents: [IntentsBitField.Flags.Guilds],
-    },
+  clientOptions: {
+    intents: [IntentsBitField.Flags.Guilds],
+  },
 
-    dev: {
-        // In testing we should enable this, it will make all our commands register in our testing guild
-        // https://jellycommands.dev/guide/commands/dev#global-dev-mode
-        global: true,
+  dev: {
+    // In testing we should enable this, it will make all our commands register in our testing guild
+    // https://jellycommands.dev/guide/commands/dev#global-dev-mode
+    global: true,
 
-        // Put your testing guild id here
-        // https://jellycommands.dev/guide/commands/dev#setup
-        guilds: [''],
-    },
+    // Put your testing guild id here
+    // https://jellycommands.dev/guide/commands/dev#setup
+    guilds: [
+      "668113742265057300", // memeresearchlab
+    ],
+  },
 });
 
 // Automatically reads the DISCORD_TOKEN environment variable
