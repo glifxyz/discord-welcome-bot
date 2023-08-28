0722a849dc4f6d07437eefcd73d4510608ab3f03
Jamie Dubs <jamie@jamiedubs.com>
2023-08-28 10:15:59 +0200

    Boilerplate from JellyCommands

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { JellyCommands } from 'jellycommands';
+import { IntentsBitField } from 'discord.js';
+
+const client = new JellyCommands({
+    // https://jellycommands.dev/guide/commands/loading
+    commands: 'src/commands',
+
+    // https://jellycommands.dev/guide/events/loading
+    events: 'src/events',
+
+    // https://jellycommands.dev/guide/buttons/loading
+    buttons: 'src/buttons',
+
+    clientOptions: {
+        intents: [IntentsBitField.Flags.Guilds],
+    },
+
+    dev: {
+        // In testing we should enable this, it will make all our commands register in our testing guild
+        // https://jellycommands.dev/guide/commands/dev#global-dev-mode
+        global: true,
+
+        // Put your testing guild id here
+        // https://jellycommands.dev/guide/commands/dev#setup
+        guilds: [''],
+    },
+});
+
+// Automatically reads the DISCORD_TOKEN environment variable
+client.login();
