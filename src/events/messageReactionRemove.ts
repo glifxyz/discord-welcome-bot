4eed2b7bed01c6b26fa5e1917bb22288c17fb4f9
Jamie Wilkinson <jamie@jamiedubs.com>
2023-09-08 13:03:28 -0400

    Don't debug messageReactionRemove

--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -2,6 +2,7 @@ import { event } from "jellycommands";
 
 export default event({
   name: "messageReactionRemove",
-  run: (_, reaction, user) =>
-    console.log("messageReactionRemove", reaction, user),
+  // run: (_, reaction, user) =>
+  //   console.log("messageReactionRemove", reaction, user),
+  run: (_, reaction, user) => {}
 });
